7528a25c30e88cdc5798c087d7ebf584a8b8c5b0
kryptondv <kryptondevpl@gmail.com>
2021-08-08 18:57:39 +0200

    added shuffle function in utils ts, added return to fetchQuizQuestions in API.ts

--- src/API.ts
+++ src/API.ts
@@ -1,8 +1,10 @@
+import { shuffle } from './utils';
+
 export type Question = {
   category: string;
   correct_answer: string;
   difficulty: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
   question: string;
   type: string;
 };
@@ -21,5 +23,8 @@ export const fetchQuizQuestions = async (
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results((question: Question) => ({
+    ...question,
+    answers: shuffle([...question.incorrect_answers, question.correct_answer]),
+  }));
 };
