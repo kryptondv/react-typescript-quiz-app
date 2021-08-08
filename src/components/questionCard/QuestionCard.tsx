7981a368c5aafab004146b557c722975e77a5b33
kryptondv <kryptondevpl@gmail.com>
2021-08-08 18:21:17 +0200

    added jsx for questionCard

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -1,5 +1,37 @@
-const QuestionCard = () => {
-  return <div>Question Card</div>;
+import React from 'react';
+
+type CardProps = {
+  question: string;
+  answers: string[];
+  cb: any;
+  userAnswer: boolean;
+  questionNr: number;
+  totalQuestions: number;
 };
+
+const QuestionCard: React.FC<CardProps> = ({
+  question,
+  answers,
+  cb,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }}></p>
+    <div>
+      {answers.map(answer => (
+        <div key={answer}>
+          <button disabled={userAnswer} onClick={cb}>
+            <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default QuestionCard;
