83bfa4f745156eac04bf112d4866a92d8ee69f45
kryptondv <kryptondevpl@gmail.com>
2021-08-08 18:10:30 +0200

    scaffolded the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Quiz</div>;
-}
+import QuestionCard from './components/questionCard/QuestionCard';
+
+const App = () => {
+  const startQuiz = async () => {};
+
+  const checkAnswer = (e: MouseEvent) => {};
+
+  const nextQuestion = () => {};
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
+      <p className="score"></p>
+      <p>Loading Questions</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
